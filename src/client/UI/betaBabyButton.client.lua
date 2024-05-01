159fdb326b81668e7adb678ceea7b58d6ad1206b
Rafplayz <rafploooz@gmail.com>
2024-05-01 15:59:18 -0400

    add a food cost to spawning child

--- src/client/UI/betaBabyButton.client.lua
+++ src/client/UI/betaBabyButton.client.lua
@@ -2,11 +2,15 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local plr = Players.LocalPlayer
+local playerStats: Folder = plr:WaitForChild("PlayerStats")
 local gui = plr.PlayerGui
 
 local babyButton: TextButton = gui:WaitForChild("GameUI"):WaitForChild("BabyButton")
 
 babyButton.Activated:Connect(function()
-	local childEvent: RemoteEvent = ReplicatedStorage.Remotes.MakeChild
-	childEvent:FireServer()
+	local childEvent: RemoteEvent = ReplicatedStorage.Shared.remotes.MakeChild
+	local Hunger: NumberValue, Thirst: NumberValue = playerStats.Hunger, playerStats.Thirst
+	if Hunger.Value >= 150 and Thirst.Value >= 250 then
+		childEvent:FireServer()
+	end
 end)
--- src/server/remoteHandlers.server.lua
+++ src/server/remoteHandlers.server.lua
@@ -3,11 +3,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local queuingPlayersModule = require(ServerScriptService.Server.modules.queuingPlayersModule)
 local initializeModule = require(ServerScriptService.Server.modules.initializePlayerModule)
 
-local remoteEvents: Folder = ReplicatedStorage.Remotes
+local remoteEvents: Folder = ReplicatedStorage.Shared.remotes
 
 local babyEvent: RemoteEvent = remoteEvents.MakeChild
 babyEvent.OnServerEvent:Connect(function(player)
-	local playerToBaby = queuingPlayersModule.getPlayerToAdd()
-	initializeModule.initPlayer(playerToBaby)
-	playerToBaby.Character:PivotTo(player.Character.PrimaryPart.CFrame)
+	if player.PlayerStats.Hunger.Value >= 150 and player.PlayerStats.Thirst.Value >= 250 then
+		local playerToBaby = queuingPlayersModule.getPlayerToAdd()
+		player.PlayerStats.Hunger.Value -= 100
+		player.PlayerStats.Thirst.Value -= 200
+		initializeModule.initPlayer(playerToBaby)
+		playerToBaby.Character:PivotTo(player.Character.PrimaryPart.CFrame)
+	end
 end)
