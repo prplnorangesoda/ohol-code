e9dde0c6371d0d959351ca1de52d7ffd5f8ca56e
Rafplayz <rafploooz@gmail.com>
2024-05-04 03:56:49 -0400

    adjust chat system

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -2,7 +2,4 @@ local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 -- init.client = StarterPlayerScripts.Client
 print("Hello world, from client!")
-
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
 Players.LocalPlayer.PlayerGui:WaitForChild("Chat").Enabled = false
