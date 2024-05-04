5c1b3f98468c3c70615ccb53c7a51aa79086bc3b
Rafplayz <rafploooz@gmail.com>
2024-05-04 03:49:24 -0400

    partial ( iwant to fork)

--- updateAssetFiles.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: undefined-global
-local map = remodel.readPlaceFile("ohol-fullymanaged.rbxl")
-
-local SERVICES = { "Workspace" }
-for _, serviceName in ipairs(SERVICES) do
-	local service = map[serviceName]
-
-	remodel.writeModelFile("assets/Workspace.rbxm", service)
-end
